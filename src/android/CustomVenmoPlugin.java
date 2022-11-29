ec3bf2a83018285f8bec610e07f2f2eae9225458
Diogo Sant'Ana <diogo.santana@wwt.com>
2022-11-29 12:25:56 -0300

    Adding braintree dependency

--- src/android/CustomVenmoPlugin.java
+++ src/android/CustomVenmoPlugin.java
@@ -2,15 +2,29 @@ package cordova.plugins;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+public interface ICustomVenmoPlugin {
+    void coolMethod(String message, CallbackContext callbackContext);
+}
+
 /**
  * This class echoes a string called from JavaScript.
  */
-public class CustomVenmoPlugin extends CordovaPlugin {
+public class CustomVenmoPlugin extends CordovaPlugin implements ICustomVenmoPlugin {
+
+    public static ICustomVenmoPlugin instance;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        instance = this;
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -22,7 +36,10 @@ public class CustomVenmoPlugin extends CordovaPlugin {
         return false;
     }
 
+    @Override
     private void coolMethod(String message, CallbackContext callbackContext) {
+        Context context = this.cordova.getActivity().getApplicationContext(); 
+
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
